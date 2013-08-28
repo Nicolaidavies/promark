a13062fa2f608294d29bfc131a34ab990a1b2bdc
Nicolaidavies <nicolai.d@hotmail.com>
2013-08-28 13:44:58 +0200

    all

---
+++ app.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+
+module LastN  # create module 
+    def last(n) # funtion to get last character of line
+        self[-n,n]
+    end
+end
+
+class String #open String class
+    include LastN #include the module in the string class
+end
+
+def boldify(line)
+    line = '<b>' + line + '</b>'
+    return line
+end
+
+def clear(line)
+    line = "<br>"
+    return line
+end
+
+get '/' do
+    erb :index
+end
+
+post '/' do
+    @input_text = params[:input_text] # read input from user
+#    counter = 1 
+    @input_text.each_line do |line|
+        line = line.strip #strip whitespace and newline
+ #       if counter > 1
+        line = '<br><br><b>' + line + '</b><br>' unless line.end_with?('.') # wrap line in bold tags if line does not end with '.'
+  #      else
+   #         line = '<b>' + line ' </b><br>' unless line.end_with?('.')
+    #    end
+        line = " " if line == "<br><br><b></b><br>" #remove bold tags, if the line is empty before entered
+        #line = line + "<br>" #add extra whitespace that was removed by rstrip
+       
+        line = '<br><b>' + line + '</b>' unless line.end_with?('.') # wrap line in bold tags if line does not end with '.'
+        line = " " if line == "<b></b>" #remove bold tags, if the line is empty before entered
+        line = line + "<br>" #add extra whitespace that was removed by rstrip
+        @output_text ||= "" #set output to an empty string if it has not been set
+        @output_text = @output_text + line # add line to string and return it
+        counter += 1
+    end
+    @output_text.strip! 
+    erb :index
+end
